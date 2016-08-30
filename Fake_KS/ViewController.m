ab28db3ec633909a89bf4931f805a0db3ef10eaf
think slowly <thinkslowlyzhang@gmail.com>
2016-08-30 14:34:59 +0800

    Initial Commit

---
+++ Fake_KS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Fake_KS
+//
+//  Created by zhang yuanan on 16/8/30.
+//  Copyright © 2016年 xiaobu network. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
